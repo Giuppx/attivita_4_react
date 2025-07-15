1236c6542408da1e298a2c9918b2b210af1ddde4
Giuseppe Lomasto <g.lomasto.94@gmail.com>
2025-07-15 12:38:59 +0200

    modifica link login

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -41,7 +41,7 @@ function Navbar() {
 						</li>
 						<li className="nav-item">
 							<Link to="login" className="nav-link">
-								login
+								Login
 							</Link>
 						</li>
 					</ul>
