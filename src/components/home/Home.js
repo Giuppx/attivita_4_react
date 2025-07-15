6758615bbd2ad92c77e81b44534e4408903fb1b1
Giuseppe Lomasto <g.lomasto.94@gmail.com>
2025-07-15 23:05:47 +0200

    definito layout componente home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,4 +1,20 @@
+import { Link } from "react-router-dom";
+
 function Home() {
-	return <>componente Home</>;
+	return (
+		<>
+			<div className="container-fluid bg-light text-dark py-5">
+				<div className="container text-center">
+					<h1 className="display-4 fw-bold">Benvenuto nella nostra App!</h1>
+					<p className="lead">
+						Scopri prodotti fantastici e crea la tua wishlist personalizzata.
+					</p>
+					<Link className="btn btn-primary mt-3" to={"/products"}>
+						Scopri i prodotti
+					</Link>
+				</div>
+			</div>
+		</>
+	);
 }
 export default Home;
