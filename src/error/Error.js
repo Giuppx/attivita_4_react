e154de9e5b623e13716b312299bb483cbb424436
Giuseppe Lomasto <g.lomasto.94@gmail.com>
2025-07-15 15:43:17 +0200

    implementaata pagina errore 404

--- src/error/Error.js
+++ src/error/Error.js
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom";
+
+import "./Error.css";
+
 function Error() {
-	return <>componente Error</>;
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-md-12 mb-4 mt-5 d-flex flex-column align-items-center justify-content-center">
+					<p className="m-0">pagina non torvata!</p>
+					<h1 className="error">404</h1>
+				</div>
+				<div className="col-md-12 d-flex justify-content-center align-items-center">
+					<Link to={"/"} className="btn btn-dark">
+						Torna alla home
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Error;
